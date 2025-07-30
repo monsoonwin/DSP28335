19d26823f2ead1cb96fd9bcc95485144993e0490
monsoonwin <149955450+monsoonwin@users.noreply.github.com>
2025-07-30 19:52:11 +0800

    Update main.c

--- ADC_seqmode/main.c
+++ ADC_seqmode/main.c
@@ -28,6 +28,8 @@ float SampleTable[BUFF_SIZE];
 void main()
 {
     int i;
+    //int arr_index;
+    //float adc_vol;
 
 	InitSysCtrl();
 
@@ -38,21 +40,30 @@ void main()
 
 	LED_Init();
 	TIM0_Init(150,200000);//200ms
+	//SMG_Init();
 	for(i=0;i<BUFF_SIZE;i++)
 	{
 	    SampleTable[i]=0;
 	}
 	ADC_Init();
+	AdcRegs.ADCTRL2.bit.SOC_SEQ1 = 1;
 
 	while(1)
 	{
 	    while (AdcRegs.ADCST.bit.INT_SEQ1== 0){}
 	        AdcRegs.ADCST.bit.INT_SEQ1_CLR = 1;
+		//adc_vol=(float)Read_ADCValue()*3.0/4096;
 		SampleTable[0] = ((AdcRegs.ADCRESULT0)>>4)*3.0/4095.0;
+		AdcRegs.ADCTRL2.bit.SOC_SEQ2 = 1;
 		while (AdcRegs.ADCST.bit.INT_SEQ2== 0){}
-		            AdcRegs.ADCST.bit.INT_SEQ2_CLR = 1;
+		AdcRegs.ADCST.bit.INT_SEQ2_CLR = 1;
 		SampleTable[1] = ((AdcRegs.ADCRESULT8)>>4)*3.0/4095.0;
 
+		AdcRegs.ADCTRL2.bit.SOC_SEQ1 = 1;
+
+
+		//SMG_DisplayFloat(adc_vol,2);
+//		SMG_DisplayInt(Read_ADCValue());
 
 	}
 }
