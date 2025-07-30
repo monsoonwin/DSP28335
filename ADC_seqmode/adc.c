85ada224b64409437e4febf87fc260546cc23d30
monsoonwin <149955450+monsoonwin@users.noreply.github.com>
2025-07-30 15:46:05 +0800

    Update adc.c

--- ADC_seqmode/adc.c
+++ ADC_seqmode/adc.c
@@ -27,21 +27,18 @@ void ADC_Init(void)
 	// Specific ADC setup for this example:
 	AdcRegs.ADCTRL1.bit.ACQ_PS = ADC_SHCLK;
 	AdcRegs.ADCTRL3.bit.ADCCLKPS = ADC_CKPS;
-	AdcRegs.ADCTRL1.bit.SEQ_CASC = 1;        // 1  Cascaded mode
+	AdcRegs.ADCTRL1.bit.SEQ_CASC = 0;        // 1  Cascaded mode
+	AdcRegs.ADCTRL3.bit.SMODE_SEL = 0;        //顺序采样
 	AdcRegs.ADCCHSELSEQ1.bit.CONV00 = 0x0;
+	AdcRegs.ADCCHSELSEQ3.bit.CONV08 = 0x8;
 	AdcRegs.ADCTRL1.bit.CONT_RUN = 1;       // Setup continuous run
-	AdcRegs.ADCMAXCONV.bit.MAX_CONV1 = 0x0;
+    AdcRegs.ADCMAXCONV.all = 0x0000;
 
 	// Start SEQ1
 	//AdcRegs.ADCTRL2.all = 0x2000;
 	AdcRegs.ADCTRL2.bit.SOC_SEQ1 = 1;
+	AdcRegs.ADCTRL2.bit.SOC_SEQ2 = 1;
 
 }
 
-Uint16 Read_ADCValue(void)
-{
-	while (AdcRegs.ADCST.bit.INT_SEQ1== 0);
-	AdcRegs.ADCST.bit.INT_SEQ1_CLR = 1;
-	return AdcRegs.ADCRESULT0>>4;
-}
 
